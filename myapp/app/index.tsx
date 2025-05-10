2863c4f5342120771116bd7cefec171b6e03a9a5
sasaiyu <sasai.y331@gmail.com>
2025-05-10 14:25:14 +0000

    buildを追加

--- myapp/app/index.tsx
+++ myapp/app/index.tsx
@@ -1,10 +1,15 @@
 import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
+import { Button } from '@expo/ui/jetpack-compose';
+
 export default function Page() {
   return (
     <View>
       <Text>Home page</Text>
       <Link href='(tabs)'>Tabs</Link>
+      <Button style={{ flex: 1 }} onPress={() => {}}>
+        Edit profile
+      </Button>
     </View>
   );
 }
